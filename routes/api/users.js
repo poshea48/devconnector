415b58ed5212fd849d12d7cb351e170a9b4933bc
Paul O'Shea <poshea48@msn.com>
2018-11-26 16:00:25 -1000

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User model
+const User = require('../../models/User');
 
 // @route GET api/users/test
 // @desc  Test users route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: "Users Works"}))
 
+// @route GET api/users/
+// @desc list of all users
+// @access Public
+router.get("/", (req, res) => {
+  let users = {};
+
+  User.find({}, (err, user) => users[user._id] = user)
+  res.status(200).send(users);
+
+})
+// @route GET api/users/register
+// @desc  register user
+// @access Public
+router .post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: "Email already exists"})
+      } else {
+        const avatar = gravatar.url()
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+  res.json({
+    name: req.body.name,
+    email: req.body.email,
+  })
+})
 module.exports = router;
