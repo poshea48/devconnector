eda8e21e8b2c34b484e01faa7e13c8329754faef
Paul O'Shea <poshea48@msn.com>
2018-12-31 14:53:41 -1000

    fixed config/keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,8 +3,3 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./keys_dev')
 }
-
-module.exports = {
-  mongoURI: 'mongodb://paul:Gators48@ds231133.mlab.com:31133/ud_devconnector',
-  secretOrKey: 'sssh-secret', // for creating jwt payload
-}
